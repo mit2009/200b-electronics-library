b37c95e990fb2293cf9fd32e099d0b62e0cc3813
Victor Hung <victorhung92@gmail.com>
2023-01-07 02:49:18 -0500

    feat: add 2.00b

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production';
-export const server = dev ? 'http://localhost:3000' : 'https://2022.2009.mit.edu';
+export const server = dev ? 'http://localhost:3000' : 'https://toy.mit.edu';

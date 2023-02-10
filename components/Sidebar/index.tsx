4f1fe78d39a1a63ab6ef917d08f70f06db8dc86c
Dillon Zhang <d1998z@gmail.com>
2023-02-09 21:39:20 -0500

    redirect urls to coming soon

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -7,6 +7,7 @@ import styles from './Sidebar.module.scss';
 
 interface IPage {
   value: string;
+  coming_soon?: boolean;
 }
 
 interface IPages { 
@@ -31,6 +32,8 @@ interface ISection extends IPage {
   chapters?: { [url: string]: IChapter };
 }
 
+const COMING_SOON_URL = "/toobers/coming-soon";
+
 const PAGES_LAYOUT: { [url: string]: ISection } = {
   "/": {
     value: "Home Icon",
@@ -51,114 +54,141 @@ const PAGES_LAYOUT: { [url: string]: ISection } = {
           },
           "/components-inside": {
             value: "The Components Inside",
+            coming_soon: true,
           }
         },
       },
       "/CAD": {
         value: "Toober Enclosure CAD",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.AT_HOME,
         has_pages: true,
         pages: {
           "/intro": {
             value: "CAD Intro",
+            coming_soon: true,
           },
           "/base-model": {
             value: "Getting the Base Model",
+            coming_soon: true,
           },
           "/case-design": {
             value: "Designing your own Case",
+            coming_soon: true,
           }
         },
       },
       "/sugar-cube": {
         value: "Sugar Cube",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.IN_LAB,
         has_pages: true,
         pages: {
           "/intro": {
             value: "Getting to know your sugar cube",
+            coming_soon: true,
           },
           "/assembly": {
             value: "Sugar Cube Assembly",
+            coming_soon: true,
           }
         },
       },
       "/battery-charger": {
         value: "Battery & Charger",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.IN_LAB,
         has_pages: true,
         pages: {
           "/intro": {
             value: "Getting to know your Battery & Charger",
+            coming_soon: true,
           },
           "/assembly": {
             value: "Battery & Charger Assembly",
+            coming_soon: true,
           }
         },
       },
       "/breadboarding": {
         value: "Breadboarding",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.AT_HOME,
         has_pages: true,
         pages: {
           "/install": {
             value: "Installing the IDE",
+            coming_soon: true,
           },
           "/test": {
             value: "Test the Microcontroller",
+            coming_soon: true,
           },
           "/circuit": {
             value: "The First Circuit",
+            coming_soon: true,
           },
           "/prototype": {
             value: "Breadboard Prototype",
+            coming_soon: true,
           }
         },
       },
       "/pcb": {
         value: "PCB Assembly",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.AT_HOME,
         has_pages: true,
         pages: {
           "/intro": {
             value: "PCB Overview",
+            coming_soon: true,
           },
           "/safety": {
             value: "Solder & Safety",
+            coming_soon: true,
           },
           "/parts": {
             value: "Preparing the Parts",
+            coming_soon: true,
           },
           "/soldering": {
             value: "Actually Soldering!",
+            coming_soon: true,
           },
           "/assembly": {
             value: "Connect it Together",
+            coming_soon: true,
           }
         },
       },
       "/final": {
         value: "Final Assembly",
+        coming_soon: true,
         due_date: "Feb 17",
         location: Location.AT_HOME,
         has_pages: true,
         pages: {
           "/intro": {
             value: "Background Info",
+            coming_soon: true,
           },
           "/prep": {
             value: "Preparing your Enclosure",
+            coming_soon: true,
           },
           "/glue": {
             value: "Glue and Adhesives",
+            coming_soon: true,
           },
           "/electronics": {
             value: "Electronics Inside",
+            coming_soon: true,
           }
         },
       },
@@ -174,7 +204,7 @@ const PageList = ({chapterUrl, pages}: {chapterUrl: string, pages?: IPages}) => 
     return (
       <ul className={styles.pageList}>
         {Object.keys(pages).map(pageUrl => {
-          return <li key={pageUrl}><Link href={chapterUrl + pageUrl}>{pages[pageUrl].value}</Link></li>
+          return <li key={pageUrl}><Link href={pages[pageUrl].coming_soon ?  COMING_SOON_URL : chapterUrl + pageUrl}>{pages[pageUrl].value}</Link></li>
         })}
       </ul>
     )
@@ -197,7 +227,7 @@ const ChapterContainer = () => {
           const chapterPath = `/${currentSection}${chapterUrl}`;
           return (
             <div className={styles.chapter} key={chapterUrl}>
-              <Link href={chapterPath}><div className={styles.chapterName}>{chapters[chapterUrl].value}</div></Link>
+              <Link href={chapters[chapterUrl].coming_soon ? COMING_SOON_URL : chapterPath}><div className={styles.chapterName}>{chapters[chapterUrl].value}</div></Link>
               <div className={styles.details}>
                 <div className={styles.location}>{chapters[chapterUrl].location}</div>
                 <div className={styles.dueDate}>{chapters[chapterUrl].due_date}</div>

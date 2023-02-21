1380f2af95935a086cc54e7a529954633f02513a
Victor Hung <victorhung92@gmail.com>
2023-02-20 19:31:19 -0500

    feat: power module assembly notes

--- pages/toobers/power/assembly.tsx
+++ pages/toobers/power/assembly.tsx
@@ -3,18 +3,28 @@ import GuideImage from '../../../components/GuideImage';
 const Page = () => {
   return (
     <div>
-      <h1>Battery & Charger Assembly</h1>
-      <h2>Still under construction</h2>
+      <h1>Power Module Assembly</h1>
+      <h2>The Battery and Charger</h2>
       <p>
-        This section will have better written instructions. But here, have some
-        photos
+        In this section, we'll assemble the power module which we'll actually
+        use, as is, in your final Toobers toy. The module is a combination of
+        two parts that we've sourced, along with a lasercut acrylic piece that
+        gives us a mounting surface and a component to attach things to. Go
+        ahead and peel the protective covering on both sides of the acrylic.
       </p>
       <GuideImage
         src="/images/toobers/power-module-peeling.jpg"
         border={false}
+        size={'MEDIUM'}
       />
+      <p>
+        Once you've peeled it and cleaned it up, take special note of the
+        difference in sides. The 'charger' text should face up and be readable.
+      </p>
       <GuideImage src="/images/toobers/power-module-crisp.jpg" border={false} />
-
+      The next piece we'll attach is a brass threaded insert. ... blah blah blah
+      explain more. This is the mcmaster part:
+      https://www.mcmaster.com/94510A360/
       <GuideImage
         src="/images/toobers/power-module-threaded-inserts.jpg"
         border={false}
@@ -31,6 +41,10 @@ const Page = () => {
         src="/images/toobers/power-module-charging-circuit.jpg"
         border={false}
       />
+      <p>
+        This section will have better written instructions. But here, have some
+        photos
+      </p>
       <GuideImage src="/images/toobers/power-module-tape.jpg" border={false} />
       <GuideImage src="/images/toobers/power-module-done.jpg" border={false} />
     </div>

cb6fb0035913a133b65321daff795908e221d367
Victor Hung <victorhung92@gmail.com>
2023-02-24 17:14:01 -0500

    feat: update a lot of instructions

--- pages/toobers/housing-cad/sweat-the-details.tsx
+++ pages/toobers/housing-cad/sweat-the-details.tsx
@@ -32,6 +32,27 @@ const Page = () => {
         soften the edges to prevent them from hurting us. We can achieve this by
         addings rounds to all the edges of our Toobers.
       </p>
+      <p>
+        To round all the edges, select all edges. There should be 16, not 12
+        (the Toober is cut in half and is actually two pieces). Click the{' '}
+        <code>Modify &gt; Fillet</code> button and give it a nice round of
+        something between 1mm and 5mm. <strong>Do not exceed 5mm</strong>. If
+        you do (for this particular edge), some of the inner features will be
+        exposed 😳!
+      </p>
+      <GuideImage
+        src={[
+          '/images/toobers/fillet-1.png',
+          '/images/toobers/fillet-2.png',
+          '/images/toobers/fillet-3.png',
+        ]}
+      />
+      <p>
+        You'll want to make sure your submission has appropriate fillets, but it
+        may not be the first thing you want to do. It's often easier to{' '}
+        <strong>make these details at the end</strong>, as it preserves straight
+        edges and lines which are easier to sketch and extrude from.
+      </p>
       <h2>The Parting Line</h2>
       <p>
         Our Toobers are assembled in two parts. Where the top and the bottom
@@ -45,7 +66,7 @@ const Page = () => {
         this line to the best of their ability, but you can usually find it.
       </p>
       <GuideImage
-        src="/images/toobers/parting-line.png"
+        src="/images/toobers/parting-line-tool.png"
         alt="Parting line on the plastic handle of a screwdriver"
       />
       <p>
@@ -59,9 +80,37 @@ const Page = () => {
       />
       <p>
         Since we cannot hide our parting line, we can choose to feature it
-        instead. We will be adding a chamfer to the edges where our two parts
-        meet up.
+        instead. In fact, oftentimes, product designers and industrial designers
+        will intentionally incorporate parting lines into the design. Below is
+        an example of an Xbox controller featuring an intentional parting line
+        design between two surfaces.
       </p>
+      <GuideImage src="/images/toobers/controller-closeup.jpg" />
+      <p>
+        For our Toobers, consider adding a very slight{' '}
+        <strong>0.2 to 0.4 mm chamfer</strong> to the edges where our two
+        housings (top and bottom) meet up.
+      </p>
+      <p>
+        Similar to the fillet tool, select all the edges you want to be
+        chamfered. In this case, it might be easier to do the top and bottom
+        housing separately (you can hide them one at a time). Here's what an
+        example Toober looks like with a <strong>2.5mm chamfer</strong> applied
+        to the seam between the two halves.
+      </p>
+      <GuideImage
+        src="/images/toobers/toober-with-chamfer.png"
+        alt="Toober with a chamfer applied to the parting line"
+        size="LARGE"
+        border={false}
+      />
+      <p>Here are some close-ups of the parting line.</p>
+      <GuideImage
+        src={[
+          '/images/toobers/toober-with-chamfer-close.png',
+          '/images/toobers/toober-with-chamfer-closer.png',
+        ]}
+      />
       <h2>A Pop of Color</h2>
       <p>
         Colors play a large role in how we percieve the world. More importantly
@@ -98,6 +147,7 @@ const Page = () => {
           click
         </li>
       </ol>
+      <GuideImage src={'/images/toobers/yellow-toober.png'} size="FULL" />
       <h2>Add a Decal</h2>
       <p>
         While we can do a lot with just color and texture, sometimes you will
@@ -142,6 +192,27 @@ const Page = () => {
           Click <code>OK</code> to accept the changes
         </li>
       </ol>
+      <p>
+        Let's say we wanted to apply this hole-filled decal image to the yellow
+        block. This image was made in <strong>illustrator</strong>, and was
+        simple as drawing a few circles (which you should have plenty of in your
+        sketchbooks as well.) This image as a <code>.png</code> such that the
+        background is transparent.
+      </p>
+      <GuideImage src={'/images/toobers/cheese.png'} size={'SMALL'} />
+      <p>
+        Following the steps above, in the decal dialog, there's also an option
+        to 'chain faces'. Sometimes it works better to leave this box unchecked
+        and select multiple faces when you apply the decal.
+      </p>
+      <GuideImage
+        src={[
+          '/images/toobers/insert-decal.png',
+          '/images/toobers/inserting-decal.png',
+          '/images/toobers/decal-done.png',
+        ]}
+      />
+      <p>And now we have the beginning of a cheese Toober!</p>
       <p>
         Please keep in mind that decals need to be re-applied when the print is
         setup. Please include no more than <strong>5 individual decals</strong>,

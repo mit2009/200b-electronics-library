bbbd474d1f8bbc35027e7097130bd7f6860cb9bc
Victor Hung <victorhung92@gmail.com>
2023-04-16 00:35:43 -0400

    feat: Add casting library information

---
+++ pages/casting-library.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>2.00b Resins & Plastics Library</h1>
+      <h2>Recommended Materials for Casting!</h2>
+      <p>More information coming soon!</p>
+    </div>
+  );
+};
+
+export default Page;

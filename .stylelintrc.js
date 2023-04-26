45b50cee1eaf505d7691eaee65331331f653f57d
Victor Hung <victorhung92@gmail.com>
2023-04-26 00:36:02 -0400

    chore: fix stylelint and add to package

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,14 +3,15 @@ const { propertyOrdering, selectorOrdering } = require('stylelint-semantic-group
 module.exports = {
   "extends": "stylelint-config-recommended",
   "plugins": ["stylelint-order", "stylelint-scss"],
+  "customSyntax": "postcss-scss",
   "rules": {
     "at-rule-no-vendor-prefix": true,
     "color-hex-length": "long",
     "at-rule-no-unknown": [
-    true,
-    {
+      true,
+      {
         "ignoreAtRules": ["for"]
-    }
+      }
     ],
     "no-descending-specificity": null,
     "string-quotes": "double",
@@ -19,7 +20,7 @@ module.exports = {
     "no-empty-source": null,
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    'order/order': selectorOrdering, 
+    'order/order': selectorOrdering,
     'order/properties-order': propertyOrdering,
   }
 }
